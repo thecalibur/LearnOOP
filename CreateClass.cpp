5830b5eb0e97c301f30789be1094bc2fbdf9bdb9
Nguyễn Quốc Hưng <87801986+thecalibur@users.noreply.github.com>
2022-06-21 09:53:08 +0700

    Create CreateClass.cpp

---
+++ CreateClass.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+class Phu_nu
+{
+
+};
+
+class Dan_ong 
+{
+
+};
+
+int main() {
+    Phu_nu thao; // Cấp phát tỉnh
+    Phu_nu *maria_ozawa = new Phu_nu; // Cấp phát động
+    Dan_ong *Bill = new Dan_ong;
+    return 0;
+}
